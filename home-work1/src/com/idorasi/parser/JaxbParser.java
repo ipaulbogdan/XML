f287afa6e7eca10f7593f6d4b669b5a209f9a2ba
Paul Idorasi <paulbogdan97@gmail.com>
2020-11-28 15:03:53 +0200

    HO2: Added schema file and JAXB validation

--- home-work1/src/com/idorasi/parser/JaxbParser.java
+++ home-work1/src/com/idorasi/parser/JaxbParser.java
@@ -5,11 +5,15 @@ import com.idorasi.pojo.Town;
 import com.idorasi.pojo.TownList;
 import com.idorasi.util.enums.Genre;
 import com.idorasi.util.enums.Zone;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
 import javax.xml.bind.JAXB;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,9 +22,10 @@ public class JaxbParser {
 
     private TownList townList;
 
-    public JaxbParser() throws JAXBException {
+    public JaxbParser() throws JAXBException, SAXException {
         JAXBContext jaxbContext = JAXBContext.newInstance(TownList.class);
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        unmarshaller.setSchema(buildSchema());
         townList = (TownList) unmarshaller.unmarshal(new File("towns.xml"));
     }
 
@@ -34,4 +39,8 @@ public class JaxbParser {
                 .collect(Collectors.toList());
     }
 
+    //adding xsd for validation
+    private Schema buildSchema() throws SAXException {
+        return SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(new File("towns.xsd"));
+    }
 }
